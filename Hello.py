bf77748f352e9c137c556191186e72646de01760
Bilal Ahmad <bilal2q2@gmail.com>
2020-04-18 00:00:54 +0500

    find the common in the two list

--- Hello.py
+++ Hello.py
@@ -5,5 +5,5 @@ def common(l1,l2):
            dummy.append(i)
     return dummy 
 list_1 = [1,2,3,4,5,6,7,8,9]
-list_2 = [2,4,6,8]
+list_2 = [2,4,6,8,9]
 print(f"The common in the list is {common(list_1,list_2)}")

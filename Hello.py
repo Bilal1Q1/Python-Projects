6b2929060353402785336cc95f1bba41bb79b467
Bilal Ahmad <bilal2q2@gmail.com>
2020-04-17 07:32:25 +0500

    project with icons

--- Hello.py
+++ Hello.py
@@ -1,1 +1,8 @@
-grocery_list = [['egges','bread','jam'],[       ],[            ]]
+grocery_list = [['egges','bread','jam'],['onion','tomato','potato'],['sugar','Ghee','tea']]
+i=0
+while i<len(grocery_list):
+    j = 0
+    while j<3:
+        print(grocery_list[][])
+        j += 1
+    i +=1

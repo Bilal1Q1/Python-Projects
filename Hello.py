67063deabad143d4f783a18d7a7697ded7ab978f
Bilal Ahmad <bilal2q2@gmail.com>
2020-04-17 23:39:50 +0500

    find the common in two lists

--- Hello.py
+++ Hello.py
@@ -1,7 +1,9 @@
-def neg_list (l):
-    nagative = []
-    for i in l:
-        nagative.append(-i)
-    return nagative
-pos_list = list(range(1,11))
-print(f"The list is {neg_list(pos_list)}")
+def common(l1,l2):
+    dummy = []
+    for i in l1:
+        if i in l2:
+           dummy.append(i)
+    return dummy 
+list_1 = [1,2,3,4,5,6,7,8,9]
+list_2 = [2,4,6,8]
+print(f"The common in the list is {common(list_1,list_2)}")
---
+++ for_just_concept.py
@@ -0,0 +1,9 @@
+name = ['Bilal','Ahmad','Atif']
+my_name = []
+if 'Bilal' in name:
+    print("Name is present")
+    my_name.append(name)
+
+else:
+    print("Name is not present")
+print(my_name)
